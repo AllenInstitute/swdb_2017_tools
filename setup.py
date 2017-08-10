e03812445c06402006b5089de12202e7899aff23
nicholasc <nicain.seattle@gmail.com>
2017-08-10 13:47:53 -0700

    migrated to cookiecutter

--- setup.py
+++ setup.py
@@ -1,24 +1,64 @@
-import setuptools
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-setuptools.setup(
-    name="swdb_2017_tools",
-    version="0.1.0",
-    url="https://github.com/AllenInstitute/swdb_2017_tools",
+"""The setup script."""
 
-    author="Allen Institute for Brain Science",
-    author_email="justink@alleninstitute.org",
+from setuptools import setup, find_packages
 
-    description="A collaborative Python package built by participants of the Summer Workshop on the Dynamic Brain",
-    long_description=open('README.rst').read(),
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
 
-    packages=setuptools.find_packages(),
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
 
-    install_requires=['allensdk'],
+requirements = [
+    'Click>=6.0',
+    # TODO: put package requirements here
+]
 
+setup_requirements = [
+    'pytest-runner',
+    # TODO(nicain): put setup requirements (distutils extensions, etc.) here
+]
+
+test_requirements = [
+    'pytest',
+    # TODO: put package test requirements here
+]
+
+setup(
+    name='swdb_2017_tools',
+    version='0.1.0',
+    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
+    long_description=readme + '\n\n' + history,
+    author="Nicholas Cain",
+    author_email='nicholasc@alleninstitute.org',
+    url='https://github.com/nicain/swdb_2017_tools',
+    packages=find_packages(include=['swdb_2017_tools']),
+    entry_points={
+        'console_scripts': [
+            'swdb_2017_tools=swdb_2017_tools.cli:main'
+        ]
+    },
+    include_package_data=True,
+    install_requires=requirements,
+    license="BSD license",
+    zip_safe=False,
+    keywords='swdb_2017_tools',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 2",
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
     ],
+    test_suite='tests',
+    tests_require=test_requirements,
+    setup_requires=setup_requirements,
 )
