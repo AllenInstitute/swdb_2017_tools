2a765f096e312c7bbdf5e8b4b006590c28dde881
forrest collman <forrest.collman@gmail.com>
2017-08-11 12:57:59 -0700

    trimming repo

--- setup.py
+++ setup.py
@@ -12,9 +12,6 @@ long_description=open('README.md').read(),
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
-with open('HISTORY.rst') as history_file:
-    history = history_file.read()
-
 with open('requirements.txt', 'r') as f:
     requirements = f.read().splitlines()
 
@@ -40,17 +37,12 @@ with open('test_requirements.txt','r') as f:
 setup(
     name='swdb_2017_tools',
     version='0.1.0',
-    description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
-    long_description=readme + '\n\n' + history,
+    description="Summer Workshop on the Dynamic Brain 2017 Tools.",
+    long_description=readme,
     author="Nicholas Cain",
     author_email='nicholasc@alleninstitute.org',
     url='https://github.com/nicain/swdb_2017_tools',
     packages=find_packages(include=['swdb_2017_tools']),
-    entry_points={
-        'console_scripts': [
-            'swdb_2017_tools=swdb_2017_tools.cli:main'
-        ]
-    },
     include_package_data=True,
     install_requires=requirements,
     license="BSD license",
@@ -61,13 +53,7 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
     ],
     test_suite='tests',
     tests_require=test_requirements
