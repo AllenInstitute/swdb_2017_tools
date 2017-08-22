af004f80540e08db04f95abc81e95ae12a5bc8ef
Elijah Christensen <ejd.christensen@gmail.com>
2017-08-21 21:50:03 -0700

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,25 @@
 from setuptools import find_packages
 
 # Add setuptools boilerplate
+setup(
+    name='swdb2017',
+
+    # Versions should comply with PEP440.  For a discussion on single-sourcing
+    # the version across setup.py and the project code, see
+    # https://packaging.python.org/en/latest/single_source_version.html
+    version='0.0.1',
+
+    description='student contributed tools from swdb2017'
+    long_description='Student tools from Summer Workshop on the Dynamic Brain 2017',
+
+    # The project's main homepage.
+    url='https://github.com/AllenInstitute/swdb_2017_tools',
+
+    # Author details
+    author='Allen Institute',
+    author_email='',
+
+    # Choose your license
+    license='MIT',
+    packages=find_packages()
+)
