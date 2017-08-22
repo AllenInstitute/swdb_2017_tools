e0a52cf9c9b4674efc8f95ed8f10186670f846db
Elijah Christensen <ejd.christensen@gmail.com>
2017-08-21 21:36:16 -0700

    package structure init

---
+++ swdb2017/observatory/roi_ops.py
@@ -0,0 +1,19 @@
+def get_roi_centroids(dataset):
+    """Extract Cell coords
+
+    Parameters
+    ----------
+    dataset : BrainObservatoryNwbDataSet
+        Allen Institute experiment file
+
+    Returns
+    -------
+    array
+      An array of tuples denoting x,y coordinates of the cell.
+    """
+    out = []
+    for mask in dataset.get_roi_mask_array():
+        y,x = mask.nonzero()
+        out.append((y.mean(),x.mean()))
+
+    return out
