538bd72db676b774c1de1107ab60c4e18a7d7b9d
Unknown <blaje42@gmail.com>
2017-08-25 18:30:31 -0400

    Added cre-lines optional input parameter

--- swdb2017/brain_observatory/has_pupil_data.py
+++ swdb2017/brain_observatory/has_pupil_data.py
@@ -1,29 +1,43 @@
-def has_pupil_data(boc,targeted_structures = None,stims = None):
-    '''Returns full dataframe with only experiments that have pupil data.
-    Input parameters:
-            boc = BrainObservatoryCache from allensdk
+import pandas as pd
+
+def has_pupil_data_df(boc,targeted_structures = None,stimuli = None,cre_lines = None):
+    '''Returns full dataframe with only experiments that have pupil data for desired targeted structures, stimuli classes, 
+    cre lines.
+    
+    Parameters
+    ----------
+            boc : BrainObservatoryCache from allensdk
             //from allensdk.core.brain_observatory_cache import BrainObservatoryCache
             //manifest_file = os.path.join(drive_path,'brain_observatory_manifest.json')
             //boc = BrainObservatoryCache(manifest_file=manifest_file)
             
-            structures = list of brain region acronyms, strings (optional)
+            targeted_structures : list
+            brain region acronyms 
             
-            stims = list of stimulus class names, strings (optional)
+            stimuli : list 
+            stimulus class names
             
-    Output parameters:
-            eye_df = dataframe containing all experiments with eye-tracking data'''
+            cre_lines : list 
+            cre-line names
+        
+    Returns
+    -------
+            eye_df : dataframe 
+            all experiments with eye-tracking data'''
     
-    #Imports
-    import pandas as pd
        
     if targeted_structures is None:
         targeted_structures = boc.get_all_targeted_structures()
-    if stims is None:
-        stims = boc.get_all_stimuli()     
+    if stimuli is None:
+        stimuli = boc.get_all_stimuli()  
+    if cre_lines is None:
+        cre_lines = boc.get_all_cre_lines()
    
-    filtered_df = pd.DataFrame(boc.get_ophys_experiments(stimuli=stims,targeted_structures = targeted_structures,simple = False))
+    filtered_df = pd.DataFrame(boc.get_ophys_experiments(stimuli=stimuli,
+                                                         targeted_structures = targeted_structures,
+                                                         cre_lines = cre_lines,
+                                                         simple = False))
 
     eye_df = filtered_df[(filtered_df['fail_eye_tracking'] == False)]
     
-    return eye_df
-    
+    return eye_df    
