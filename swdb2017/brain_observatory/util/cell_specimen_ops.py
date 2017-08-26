5894a9a1556be586c90ce70a2bacc0a603566387
Elijah Christensen <ejd.christensen@gmail.com>
2017-08-26 09:54:06 -0700

    refactor to make series instead of string

--- swdb2017/brain_observatory/util/cell_specimen_ops.py
+++ swdb2017/brain_observatory/util/cell_specimen_ops.py
@@ -25,6 +25,7 @@ def get_run_mod_cells(boc,stimuli='natural_scenes',p_max=1):
         (cell_df[run_mod_cols[stimuli][1]].notnull())
         & (cell_df[run_mod_cols[stimuli][0]]<p_max)
     ]
-    run_cells['run_mod_valence'] = ['amplified' if mod>0 else 'suppressed' for mod in run_cells[run_mod_cols[stimuli][1]]]
+    valence = pd.Series(['amplified' if mod>0 else 'suppressed' for mod in run_cells[run_mod_cols[stimuli][1]]])
 
+    run_cells['run_mod_valence'] = valence
     return run_cells
