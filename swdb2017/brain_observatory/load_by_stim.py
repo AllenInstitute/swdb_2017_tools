4f1bac20a52a666f9fae3c625f04dd13ee031009
charlie <charlieheller95@gmail.com>
2017-08-26 20:16:33 -0700

    add functions for plotting linear gradient, modify load functions to return cellids

--- swdb2017/brain_observatory/load_by_stim.py
+++ swdb2017/brain_observatory/load_by_stim.py
@@ -14,6 +14,10 @@ def get_spont_specific_fluorescence_traces(exp, raw):
     # pr_df: A data frame object containing the trial sorted pupil size for all
     # different natural scenes presented
 
+    # cell_ids: numpy array containing unique cell ids. Index of cell id matches
+    # with index of cell X time matrix contained inside dff_df
+
+    cell_ids = exp.get_cell_specimen_ids()
     if raw:
         t, dff = exp.get_fluorescence_traces()  # Read in calcium signal
     else:
@@ -37,7 +41,7 @@ def get_spont_specific_fluorescence_traces(exp, raw):
     dff_df = pd.DataFrame(data=dff_temp, columns=columns)
     columns = sorted(pr_temp.keys())
     pr_df = pd.DataFrame(data=pr_temp, columns=columns)
-    return dff_df, pr_df
+    return dff_df, pr_df, cell_ids
 
 def get_ns_specific_fluorescence_traces(exp, raw):
 
@@ -51,6 +55,11 @@ def get_ns_specific_fluorescence_traces(exp, raw):
 
     # pr_df: A data frame object containing the trial sorted pupil size for all
     # different natural scenes presented
+
+    # cell_ids: numpy array containing unique cell ids. Index of cell id matches
+    # with index of cell X time matrix contained inside dff_df
+
+    cell_ids = exp.get_cell_specimen_ids()
 
     if raw:
         t, dff = exp.get_fluorescence_traces()  # Read in calcium signal
@@ -67,7 +76,7 @@ def get_ns_specific_fluorescence_traces(exp, raw):
     pr_temp = dict()
     for i, u_s in enumerate(unique_stim):
         start = stim_table['start'][stim_table['frame']==u_s].values
-        end = stim_table['end'][stim_table['frame']==u_s].values
+        end = start + 7#stim_table['end'][stim_table['frame']==u_s].values
         dff_temp[u_s] = []
         pr_temp[u_s] = []
         for j in range(0, len(start)):
@@ -77,4 +86,4 @@ def get_ns_specific_fluorescence_traces(exp, raw):
     dff_df = pd.DataFrame(data=dff_temp, columns=columns)
     columns = sorted(pr_temp.keys())
     pr_df = pd.DataFrame(data=pr_temp, columns = columns)
-    return dff_df, pr_df
+    return dff_df, pr_df, cell_ids
