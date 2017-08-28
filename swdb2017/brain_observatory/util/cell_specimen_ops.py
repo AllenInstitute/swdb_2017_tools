78acb621762ac2cc4c9f87b8ddcaf1b1f502d7f5
Elijah Christensen <ejd.christensen@gmail.com>
2017-08-28 09:13:24 -0700

    removed valence column addition

--- swdb2017/brain_observatory/util/cell_specimen_ops.py
+++ swdb2017/brain_observatory/util/cell_specimen_ops.py
@@ -21,11 +21,9 @@ def get_run_mod_cells(boc,stimuli='natural_scenes',p_max=1):
         drifting_gratings=['p_run_mod_dg','run_mod_dg'],
         natural_scenes=['p_run_mod_ns','run_mod_ns']
     )
+
     run_cells = cell_df[
         (cell_df[run_mod_cols[stimuli][1]].notnull())
         & (cell_df[run_mod_cols[stimuli][0]]<p_max)
     ]
-    valence = pd.Series(['amplified' if mod>0 else 'suppressed' for mod in run_cells[run_mod_cols[stimuli][1]]])
-
-    run_cells[u'run_mod_valence'] = valence
     return run_cells
