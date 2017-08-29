bba78172cc56bf68ed6914d116306f1305a1d577
charlie <charlieheller95@gmail.com>
2017-08-28 23:26:38 -0700

    tweaked

--- swdb2017/brain_observatory/util/pearson_corr_coeff.py
+++ swdb2017/brain_observatory/util/pearson_corr_coeff.py
@@ -13,18 +13,10 @@ def pearson_corr_coeff(x, y):
     c: The pearson correlation coefficient between x and y
 
     '''
-
-    if y != None:
         x_diffs = np.ediff1d(x)
         y_diffs = np.ediff1d(y)
         num = np.dot(x_diffs,y_diffs)
         den = (np.sqrt(sum(x_diffs**2)*sum(y_diffs**2)))
         c = num/den
 
-    else:
-        x_diffs = np.ediff1d(x)
-        y_diffs = np.ediff1d(x)
-        num = np.dot(x_diffs,y_diffs)
-        den = (np.sqrt(sum(x_diffs**2)*sum(y_diffs**2)))
-        c = num/den
     return c
