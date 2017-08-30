71fbccf65b914be11eb59b269dff8fad7d86b249
charlie <charlieheller95@gmail.com>
2017-08-30 15:57:59 -0700

    debug correlation function

--- projects/trash_pandas/pearson_corr_coeff.py
+++ projects/trash_pandas/pearson_corr_coeff.py
@@ -18,14 +18,16 @@ def pearson_corr_coeff(x, y):
     corr_coef: int
         The pearson correlation coefficient between x and y
     '''
-
+    x = x.astype(np.float64)
+    y = y.astype(np.float64)
     x_inds = np.where(np.isnan(x))[0]
     y_inds = np.where(np.isnan(y))[0]
     nan_inds = np.sort(np.unique(np.concatenate((x_inds, y_inds))))
-    x = np.delete(x, nan_inds)
-    y = np.delete(y, nan_inds)
 
-    corr_mat = np.corrcoef(x, y)
+    x_no_nan = np.delete(x, nan_inds)
+    y_no_nan = np.delete(y, nan_inds)
+
+    corr_mat = np.corrcoef(x_no_nan, y_no_nan)
     corr_coef = corr_mat[1, 0]
 
     return corr_coef
